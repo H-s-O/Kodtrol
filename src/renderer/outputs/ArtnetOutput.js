b10cec6a38ca06d33a0e9b0334f3f584014efce4
Hugo St-Onge <hugostonge@gmail.com>
2018-09-06 21:53:10 -0400

    optimize?

--- src/renderer/outputs/ArtnetOutput.js
+++ src/renderer/outputs/ArtnetOutput.js
@@ -11,15 +11,26 @@ export default class ArtnetOutput {
   }
   
   send = (data) => {
-    const outputArr = [];
-    for (let i = 0; i < 512; i++) {
-      if (data && i in data) {
-        let val = data[i];
+    // const outputArr = [];
+    // for (let i = 0; i < 512; i++) {
+    //   if (data && i in data) {
+    //     let val = data[i];
+    //     val = val < 0 ? 0 : val > 255 ? 255 : val;
+    //     outputArr[i] = val >> 0;
+    //   } else {
+    //     outputArr[i] = 0;
+    //   }
+    // }
+    let outputArr;
+    if (data) {
+      outputArr = Object.entries(data).reduce((arr, [channel, value]) => {
+        let val = value;
         val = val < 0 ? 0 : val > 255 ? 255 : val;
-        outputArr[i] = val >> 0;
-      } else {
-        outputArr[i] = 0;
-      }
+        arr[Number(channel)] = val >> 0;
+        return arr;
+      }, []);
+    } else {
+      outputArr = [];
     }
     
     this.output.set(0, outputArr);
