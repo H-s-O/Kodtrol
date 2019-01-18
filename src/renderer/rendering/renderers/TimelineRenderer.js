31a6a96477d82bb9247c877285334a43459bcd34
Hugo St-Onge <hugostonge@gmail.com>
2019-01-17 23:05:50 -0500

    WIP timemap

--- src/renderer/rendering/renderers/TimelineRenderer.js
+++ src/renderer/rendering/renderers/TimelineRenderer.js
@@ -14,6 +14,7 @@ export default class TimelineRenderer {
   _triggers = null;
   _curves = null;
   _audios = null;
+  _timeMap = null;
   
   constructor(providers, timelineId) {
     this._providers = providers;
@@ -39,39 +40,126 @@ export default class TimelineRenderer {
     
     this._blocks = timelineItems
       .filter((item) => 'script' in item)
-      .map((block) => {
+      .reduce((obj, block) => {
         return {
-          ...block,
-          instance: new ScriptRenderer(this._providers, block.script, false),
+          ...obj,
+          [block.id]: {
+            ...block,
+            instance: new ScriptRenderer(this._providers, block.script, false),
+          },
         };
-      });
+      }, {});
       
     this._audios = timelineItems
       .filter((item) => 'file' in item)
-      .map((audio) => {
+      .reduce((obj, audio) => {
         return {
-          ...audio,
-          instance: new AudioRenderer(this._providers, audio),
+          ...obj,
+          [audio.id]: {
+            ...audio,
+            instance: new AudioRenderer(this._providers, audio),
+          },
         };
-      });
+      }, {});
     
     this._triggers = timelineItems
       .filter((item) => 'trigger' in item)
-      .map((trigger) => {
+      .reduce((obj, trigger) => {
         return {
-          ...trigger,
-          instance: new TriggerRenderer(),
+          ...obj,
+          [trigger.id]: {
+            ...trigger,
+            instance: new TriggerRenderer(),
+          },
         };
-      });
+      }, {});
       
     this._curves = timelineItems
       .filter((item) => 'curve' in item)
-      .map((curve) => {
+      .reduce((obj, curve) => {
         return {
-          ...curve,
-          instance: new CurveRenderer(curve),
+          ...obj,
+          [curve.id]: {
+            ...curve,
+            instance: new CurveRenderer(curve),
+          },
         };
-      });
+      }, {});
+    
+    // Warning, experimental stuff below  
+    const timeMap = [];
+    const duration = this._timeline.duration;
+    const itemsCount = timelineItems.length;
+    const divisor = 1000;
+    for (let t = 0; t < duration; t += divisor) {
+      const timeIndex = (t / divisor) >> 0;
+      const end = t + divisor;
+      for (let i = 0; i < itemsCount; i++) {
+        const item = timelineItems[i];
+        const { id, inTime, outTime, script, file, trigger, curve } = item;
+        let addIt = false;
+        if (typeof outTime !== 'undefined') {
+          if (
+              (inTime >= t && inTime <= end)
+              || (outTime >= t && outTime <= end)
+              || (inTime < t && outTime > end)
+            ) {
+            addIt = true;
+          }
+        } else if (inTime >= t && inTime <= end) {
+          addIt = true;
+        }
+        if (addIt) {
+          let addIndex = null;
+          if (typeof script !== 'undefined') {
+            addIndex = 2;
+          } else if (typeof file !== 'undefined') {
+            addIndex = 3;
+          } else if (typeof trigger !== 'undefined') {
+            addIndex = 0;
+          } else if (typeof curve !== 'undefined') {
+            addIndex = 1;
+          } 
+          if (addIndex !== null) {
+            if (!timeMap[timeIndex]) {
+              timeMap[timeIndex] = [
+                [], // triggers
+                [], // curves
+                [], // blocks
+                [], // medias
+              ];
+            }
+            timeMap[timeIndex][addIndex].push(id);
+          }
+        }
+      }
+    }
+    this._timeMap = timeMap;
+    
+    // this._timeMap = timelineItems.reduce((arr, item) => {
+    //   const { id, inTime, script, file, trigger, curve } = item;
+    //   const index = Math.floor(inTime / 1000);
+    //   if (!arr[index]) {
+    //     arr[index] = [
+    //       [], // triggers
+    //       [], // curves
+    //       [], // blocks
+    //       [], // medias
+    //     ];
+    //   }
+    //   if (script) {
+    //     arr[index][2].push(id);
+    //   } else if (file) {
+    //     arr[index][3].push(id);
+    //   } else if (trigger) {
+    //     arr[index][0].push(id);
+    //   } else if (curve) {
+    //     arr[index][1].push(id);
+    //   } 
+    //   return arr;
+    // }, []);
+    
+    console.log(this._timeMap);
   }
   
   get rendererType() {
@@ -104,11 +192,21 @@ export default class TimelineRenderer {
       // @TODO
       return;
     }
+    
+    const timeMap = this._timeMap;
+    const timeDivisor = 1000;
+    const timeIndex = (currentTime / timeDivisor) >> 0;
+    if (timeIndex > timeMap.length || !timeMap[timeIndex]) {
+      // Nothing to render
+      return;
+    }
+    const timeItems = timeMap[timeIndex];
 
-    const triggerData = this._triggers
+    const triggerData = timeItems[0]
+      .map((id) => this._triggers[id])
       .filter((trigger) => (
         currentTime >= trigger.inTime
-        && currentTime <= trigger.inTime + 30
+        && currentTime <= trigger.inTime + 25 // 1 frame
         && !trigger.instance.triggered
       ))
       .reduce((renderTriggerData, trigger) => {
@@ -120,7 +218,8 @@ export default class TimelineRenderer {
         };
       }, {});
 
-    const curveData = this._curves
+    const curveData = timeItems[1]
+      .map((id) => this._curves[id])
       .filter((curve) => (
         currentTime >= curve.inTime
         && currentTime <= curve.outTime
@@ -142,7 +241,8 @@ export default class TimelineRenderer {
         };
       }, {});
 
-    this._blocks
+    timeItems[2]
+      .map((id) => this._blocks[id])
       .filter((block) => (
         currentTime >= block.inTime - 500 // @TODO config script setup delay
         && currentTime <= block.outTime
@@ -159,7 +259,8 @@ export default class TimelineRenderer {
         block.instance.render(currentTime, blockInfo, triggerData, curveData);
       }, {});
     
-    this._audios
+    timeItems[3]
+      .map((id) => this._audios[id])
       .filter((audio) => (
         currentTime >= audio.inTime
         && currentTime <= audio.outTime
@@ -178,29 +279,29 @@ export default class TimelineRenderer {
   }
 
   beat = (beat, delta) => {
-    const currentTime = this._currentTime + delta;
-    // console.log(delta);
-    this._blocks
-      .filter((block) => (
-        currentTime >= block.inTime
-        && currentTime <= block.outTime
-      ))
-      .forEach((block) => {
-        block.instance.beat(beat, currentTime);
-      });
+    // const currentTime = this._currentTime + delta;
+    // // console.log(delta);
+    // this._blocks
+    //   .filter((block) => (
+    //     currentTime >= block.inTime
+    //     && currentTime <= block.outTime
+    //   ))
+    //   .forEach((block) => {
+    //     block.instance.beat(beat, currentTime);
+    //   });
   }
   
   input = (type, data) => {
-    const currentTime = this._currentTime;
-    
-    this._blocks
-      .filter((block) => (
-        currentTime >= block.inTime
-        && currentTime <= block.outTime
-      ))
-      .forEach((block) => {
-        block.instance.input(type, data);
-      });
+    // const currentTime = this._currentTime;
+    // 
+    // this._blocks
+    //   .filter((block) => (
+    //     currentTime >= block.inTime
+    //     && currentTime <= block.outTime
+    //   ))
+    //   .forEach((block) => {
+    //     block.instance.input(type, data);
+    //   });
   }
 
   restartTimeline = () => {
@@ -211,24 +312,24 @@ export default class TimelineRenderer {
   }
 
   resetBlocks = () => {
-    this._blocks.forEach((block) => block.instance.reset());
+    Object.values(this._blocks).forEach((block) => block.instance.reset());
   }
 
   resetTriggers = () => {
-    this._triggers.forEach((trigger) => trigger.instance.reset());
+    Object.values(this._triggers).forEach((trigger) => trigger.instance.reset());
   }
 
   resetCurves = () => {
-    this._curves.forEach((curve) => curve.instance.reset());
+    Object.values(this._curves).forEach((curve) => curve.instance.reset());
   }
 
   resetAudios = () => {
-    this._audios.forEach((audio) => audio.instance.reset());
+    Object.values(this._audios).forEach((audio) => audio.instance.reset());
   }
 
   destroy = () => {
-    this._blocks.forEach((block) => block.instance.destroy());
-    this._curves.forEach((curve) => curve.instance.destroy());
+    Object.values(this._blocks).forEach((block) => block.instance.destroy());
+    Object.values(this._curves).forEach((curve) => curve.instance.destroy());
 
     this.blocks = null;
     this.triggers = null;
