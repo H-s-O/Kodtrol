362049830355aa77252a08fbd859cc47e521bc4d
Hugo St-Onge <hugostonge@gmail.com>
2019-05-29 19:42:17 -0400

    WIP

--- src/renderer/outputs/ArtnetOutput.js
+++ src/renderer/outputs/ArtnetOutput.js
@@ -1,46 +1,24 @@
-import artnet from 'artnet';
+import DMX from 'dmx';
 
 export default class ArtnetOutput {
   output = null;
   
   constructor(address) {
-    this.output = artnet({
-      host: address,
-      sendAll: true, // skips sending/updating some channels without this :(
-    });
+    this.output = new DMX();
+    this.output.addUniverse('main', 'artnet', address);
     console.log('Art-Net output');
   }
   
   send = (data) => {
-    // const outputArr = [];
-    // for (let i = 0; i < 512; i++) {
-    //   if (data && i in data) {
-    //     let val = data[i];
-    //     val = val < 0 ? 0 : val > 255 ? 255 : val;
-    //     outputArr[i] = val >> 0;
-    //   } else {
-    //     outputArr[i] = 0;
-    //   }
-    // }
-    let outputArr;
-    if (data) {
-      outputArr = Object.entries(data).reduce((arr, [channel, value]) => {
-        let val = value;
-        val = val < 0 ? 0 : val > 255 ? 255 : val;
-        arr[Number(channel)] = val >> 0;
-        return arr;
-      }, []);
-    } else {
-      outputArr = [];
-    }
-    
-    this.output.set(0, outputArr);
+    this.output.update('main', data);
   }
   
   destroy = () => {
     if (this.output) {
-      this.output.close();
+      // Manually stop universes
+      Object.values(this.output.universes).forEach((universe) => universe.stop());
     }
+    
     this.output = null;
   }
 }
