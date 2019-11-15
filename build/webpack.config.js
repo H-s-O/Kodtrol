be9319d6d7df57c140da37fccdcbabcfaf6067bd
Hugo St-Onge <hugostonge@gmail.com>
2019-11-15 14:40:20 -0500

    update webpack loaders

--- build/webpack.config.js
+++ build/webpack.config.js
@@ -56,7 +56,6 @@ module.exports = {
             loader: 'css-loader',
             options: {
               modules: true,
-              localIdentName: '[path][name]__[local]--[hash:base64:5]'
             },
           },
           {
