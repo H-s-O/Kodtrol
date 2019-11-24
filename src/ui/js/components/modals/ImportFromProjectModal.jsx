40360f76fe3f9f7c14a12b4b25d07fede785a29a
Hugo St-Onge <hugostonge@gmail.com>
2019-11-23 22:00:46 -0500

    temp fix

--- src/ui/js/components/modals/ImportFromProjectModal.jsx
+++ src/ui/js/components/modals/ImportFromProjectModal.jsx
@@ -21,7 +21,6 @@ export default function ImportFromProjectModal(props) {
             field: 'device',
             type: 'select',
             from: 'items',
-            hideEmpty: true,
           },
         ]}
     />
