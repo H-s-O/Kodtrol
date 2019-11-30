66f7fcbd623f24460747f7b46383444a00c80560
Hugo St-Onge <hugostonge@gmail.com>
2019-11-29 20:01:20 -0500

    saving a device or media should trigger a project save

--- src/common/js/store/middlewares/saveableContentCallback.js
+++ src/common/js/store/middlewares/saveableContentCallback.js
@@ -5,13 +5,15 @@ export default (callback) => {
   return store => next => action => {
     const result = next(action);
     if (
-      action.type === 'SAVE_SCRIPT'
+      action.type === 'SAVE_DEVICE'
+      || action.type === 'SAVE_SCRIPT'
       || action.type === 'SAVE_TIMELINE'
       || action.type === 'SAVE_BOARD'
       || action.type === 'SAVE_INPUT'
       || action.type === 'SAVE_OUTPUT'
       || action.type === 'SAVE_INPUTS'
       || action.type === 'SAVE_OUTPUTS'
+      || action.type === 'SAVE_MEDIA'
     ) {
       callback(action.type);
     }
