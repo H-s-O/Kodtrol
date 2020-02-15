836ed1e53077166ea30c78e5e0bb977fc20429a5
Hugo St-Onge <hugostonge@gmail.com>
2020-02-15 17:00:33 -0500

    fix script hashing

--- src/common/js/store/reducers/saveEditedScript.js
+++ src/common/js/store/reducers/saveEditedScript.js
@@ -1,4 +1,10 @@
 import { SAVE_EDITED_SCRIPT } from '../actions/scripts';
+import { hashDataObject } from '../../lib/hash';
+
+const excludeHashProps = [
+  'id',
+  'name',
+];
 
 export default (state, { type, payload }) => {
   switch (type) {
@@ -7,7 +13,17 @@ export default (state, { type, payload }) => {
       return {
         ...state,
         editScripts: state.editScripts.map((script) => script.id === payload ? { ...script, changed: false } : script),
-        scripts: state.scripts.map((script) => script.id === payload ? { ...script, content } : script),
+        scripts: state.scripts.map((script) => {
+          if (script.id === payload) {
+            const newData = { ...script, content };
+            const hash = hashDataObject(newData, excludeHashProps);
+            return {
+              ...newData,
+              hash,
+            };
+          }
+          return script;
+        }),
       };
     }
       break;
