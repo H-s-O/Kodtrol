f179eee7382a5408b905292a5e00a89380e15b05
Hugo St-Onge <hugostonge@gmail.com>
2020-02-16 13:54:17 -0500

    fix saveable content middleware

--- src/common/js/store/middlewares/saveableContentCallback.js
+++ src/common/js/store/middlewares/saveableContentCallback.js
@@ -1,19 +1,22 @@
-import { stopTimeline } from '../actions/timelines';
-import { stopPreviewScript } from '../actions/scripts';
+import { SAVE_DEVICE } from '../actions/devices';
+import { SAVE_SCRIPT } from '../actions/scripts';
+import { SAVE_TIMELINE } from '../actions/timelines';
+import { SAVE_MEDIA } from '../actions/medias';
+import { SAVE_BOARD } from '../actions/boards';
+import { SAVE_INPUTS } from '../actions/inputs';
+import { SAVE_OUTPUTS } from '../actions/outputs';
 
 export default (callback) => {
   return store => next => action => {
     const result = next(action);
     if (
-      action.type === 'SAVE_DEVICE'
-      || action.type === 'SAVE_SCRIPT'
-      || action.type === 'SAVE_TIMELINE'
-      || action.type === 'SAVE_BOARD'
-      || action.type === 'SAVE_INPUT'
-      || action.type === 'SAVE_OUTPUT'
-      || action.type === 'SAVE_INPUTS'
-      || action.type === 'SAVE_OUTPUTS'
-      || action.type === 'SAVE_MEDIA'
+      action.type === SAVE_DEVICE
+      || action.type === SAVE_SCRIPT
+      || action.type === SAVE_MEDIA
+      || action.type === SAVE_TIMELINE
+      || action.type === SAVE_BOARD
+      || action.type === SAVE_INPUTS
+      || action.type === SAVE_OUTPUTS
     ) {
       callback(action.type);
     }
