2ad59832daa17f7dd12c02e9a8c1103383b92d3a
Hugo St-Onge <hugostonge@gmail.com>
2020-03-07 22:13:45 -0500

    "create" actions should trigger save

--- src/common/js/store/middlewares/saveableContentCallback.js
+++ src/common/js/store/middlewares/saveableContentCallback.js
@@ -1,26 +1,33 @@
-import { SAVE_DEVICE } from '../actions/devices';
-import { SAVE_SCRIPT, SAVE_EDITED_SCRIPT } from '../actions/scripts';
-import { SAVE_TIMELINE, SAVE_EDITED_TIMELINE } from '../actions/timelines';
-import { SAVE_MEDIA } from '../actions/medias';
-import { SAVE_BOARD, SAVE_EDITED_BOARD } from '../actions/boards';
+import { SAVE_DEVICE, CREATE_DEVICE } from '../actions/devices';
+import { SAVE_SCRIPT, SAVE_EDITED_SCRIPT, CREATE_SCRIPT } from '../actions/scripts';
+import { SAVE_TIMELINE, SAVE_EDITED_TIMELINE, CREATE_TIMELINE } from '../actions/timelines';
+import { SAVE_MEDIA, CREATE_MEDIA } from '../actions/medias';
+import { SAVE_BOARD, SAVE_EDITED_BOARD, CREATE_BOARD } from '../actions/boards';
 import { SAVE_INPUTS } from '../actions/inputs';
 import { SAVE_OUTPUTS } from '../actions/outputs';
+
+const SAVEABLE_ACTIONS = [
+  CREATE_DEVICE,
+  SAVE_DEVICE,
+  CREATE_SCRIPT,
+  SAVE_SCRIPT,
+  SAVE_EDITED_SCRIPT,
+  CREATE_MEDIA,
+  SAVE_MEDIA,
+  CREATE_TIMELINE,
+  SAVE_TIMELINE,
+  SAVE_EDITED_TIMELINE,
+  CREATE_BOARD,
+  SAVE_BOARD,
+  SAVE_EDITED_BOARD,
+  SAVE_INPUTS,
+  SAVE_OUTPUTS,
+];
 
 export default (callback) => {
   return store => next => action => {
     const result = next(action);
-    if (
-      action.type === SAVE_DEVICE
-      || action.type === SAVE_SCRIPT
-      || action.type === SAVE_EDITED_SCRIPT
-      || action.type === SAVE_MEDIA
-      || action.type === SAVE_TIMELINE
-      || action.type === SAVE_EDITED_TIMELINE
-      || action.type === SAVE_BOARD
-      || action.type === SAVE_EDITED_BOARD
-      || action.type === SAVE_INPUTS
-      || action.type === SAVE_OUTPUTS
-    ) {
+    if (SAVEABLE_ACTIONS.includes(action.type)) {
       callback(action.type);
     }
     return result;
