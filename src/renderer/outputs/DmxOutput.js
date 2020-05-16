4c056a7f5ad569e1c5a3279d7495431cb6d2f12e
Hugo St-Onge <hugostonge@gmail.com>
2020-05-16 15:32:25 -0400

    DmxOutput: quick hack to prevent "Cannot lock port" errors

--- src/renderer/outputs/DmxOutput.js
+++ src/renderer/outputs/DmxOutput.js
@@ -6,6 +6,8 @@ export default class DmxOutput extends AbstractOutput {
   _output = null;
   _driver = null;
   _port = null;
+  _canCheck = false;
+  _checkTimeout = null;
 
   constructor(driver, port) {
     super();
@@ -17,12 +19,14 @@ export default class DmxOutput extends AbstractOutput {
   }
 
   _create() {
+    clearTimeout(this._checkTimeout);
     this._destroyOutput();
 
     try {
       this._output = new DMX();
       this._output.addUniverse('main', this._driver, this._port);
       console.log('DmxOutput _create()', this._driver, this._port);
+      this._checkTimeout = setTimeout(() => this._canCheck = true, 3000);
     } catch (e) {
       console.error('DmxOutput _create() error', e);
       this._setStatusDisconnected();
@@ -44,7 +48,7 @@ export default class DmxOutput extends AbstractOutput {
   }
 
   _refreshStatus() {
-    if (!this._output) {
+    if (!this._output || !this._canCheck) {
       this._setStatusInitial();
       return;
     }
@@ -73,6 +77,8 @@ export default class DmxOutput extends AbstractOutput {
   }
 
   _destroyOutput() {
+    clearTimeout(this._checkTimeout);
+
     if (this._output) {
       // Manually stop universes
       Object.values(this._output.universes).forEach((universe) => {
@@ -89,6 +95,10 @@ export default class DmxOutput extends AbstractOutput {
     this._destroyOutput();
 
     this._output = null;
+    this._driver = null;
+    this._port = null;
+    this._canCheck = null;
+    this._checkTimeout = null;
 
     super.destroy();
   }
