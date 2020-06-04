b60a0cdd27620f132f6a9836f73f086f37a45259
Hugo St-Onge <hugostonge@gmail.com>
2020-06-03 21:16:03 -0400

    fix for bug introduced in previous commit that made localBeat miss the 0 beat

--- src/renderer/rendering/renderers/root/RootTimelineRenderer.js
+++ src/renderer/rendering/renderers/root/RootTimelineRenderer.js
@@ -318,11 +318,15 @@ export default class RootTimelineRenderer extends BaseRootRenderer {
       ) {
         const localBeatPos = timeToPPQ(currentTime - block.inTime, tempo);
         if (localBeatPos !== block.localBeatPos) {
-          // Loop the difference between two positions; will act
-          // as catch-up in case some lag occurs
-          const diff = localBeatPos - block.localBeatPos;
-          for (let i = 1; i < diff + 1; i++) {
-            block.instance.beat(beatPos, block.localBeatPos + i);
+          if (block.localBeatPos === -1) {
+            block.instance.beat(beatPos, localBeatPos);
+          } else {
+            // Loop the difference between two positions; will act
+            // as catch-up in case some lag occurs
+            const diff = localBeatPos - block.localBeatPos;
+            for (let i = 1; i < diff + 1; i++) {
+              block.instance.beat(beatPos, block.localBeatPos + i);
+            }
           }
           block.localBeatPos = localBeatPos;
         }
