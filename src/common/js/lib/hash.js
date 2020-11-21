a4d9ffebcfc61557824e1d1203e1a58238969a2f
H-s-O <hugostonge@gmail.com>
2020-11-20 21:50:15 -0500

    quick patch for object-hash module; fix moving layers in timelines and boards not being updated on save

--- src/common/js/lib/hash.js
+++ src/common/js/lib/hash.js
@@ -1,13 +1,29 @@
 import objectHash from 'object-hash';
 
+const excluder = (exclusions, key, depth) => {
+  if (!(key in exclusions)) {
+    return false;
+  }
+  if (exclusions[key] === true) {
+    return true;
+  }
+  return exclusions[key] === depth;
+}
+
 export const hashDataObject = (value, exclude = []) => {
-  const exclusions = [...exclude, 'hash'];
+  const exclusions = [...exclude, 'hash'].reduce((obj, key) => {
+    if (key.indexOf(':') !== -1) {
+      const [cleanKey, depth] = key.split(':')
+      return { ...obj, [cleanKey]: parseInt(depth) };
+    }
+    return { ...obj, [key]: true };
+  }, {});
   const newHash = objectHash(value, {
     algorithm: 'md5',
     ignoreUnknown: true,
     respectType: false,
     unorderedArrays: true,
-    excludeKeys: (key) => exclusions.indexOf(key) !== -1,
+    excludeKeys: (key, depth) => excluder(exclusions, key, depth),
   });
   return newHash;
 };
