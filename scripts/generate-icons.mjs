4ec922c7ad99cf8dcb7583babfb70b69d8101bf2
H-s-O <hugostonge@gmail.com>
2023-12-04 20:29:16 -0500

    Use icon-gen instead of electron-icon-builder (which itself relied on the deprecated phantomjs)

---
+++ scripts/generate-icons.mjs
@@ -0,0 +1,24 @@
+import icongen from 'icon-gen';
+import { join, dirname } from 'path';
+import { mkdir } from 'fs/promises';
+import { fileURLToPath } from 'url';
+
+const ROOT_DIR = join(dirname(fileURLToPath(import.meta.url)), '..')
+
+try {
+  await mkdir(join(ROOT_DIR, 'build', 'icons'), { recursive: true });
+
+  await icongen(join(ROOT_DIR, 'src', 'icons', 'app.png'), join(ROOT_DIR, 'build', 'icons'), {
+    report: true,
+    favicon: null,
+    ico: {
+      name: 'app',
+    },
+    icns: {
+      name: 'app',
+    },
+  });
+} catch (err) {
+  console.error(err);
+  process.exit(1);
+}
